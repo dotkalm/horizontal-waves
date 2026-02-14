9872e6f290b5b806f4446df4b752388a6d5a5bf1
dotkalm <joelmholmberg@gmail.com>
2026-02-14 11:48:34 -0500

    event handler

--- src/components/Viewbox/index.tsx
+++ src/components/Viewbox/index.tsx
@@ -1,4 +1,7 @@
-import { Index } from 'solid-js';
+import { 
+  Index,
+  createSignal,
+} from 'solid-js';
 import { styled } from 'solid-styled-components';
 import { zigZagPath } from '~/utils';
 
@@ -14,19 +17,42 @@ const PATH_COUNT = 30;
 
 export default function Viewbox() {
   const spacing = VIEWBOX_HEIGHT / (PATH_COUNT + 1);
+  const [isPressed, setIsPressed] = createSignal(false);
+  const handleMouseEnter = () => {
+    // figure out what path we are hovering over and then understand the x position 
+  }
 
   return (
     <StyledSvg viewBox={`0 0 ${VIEWBOX_WIDTH} ${VIEWBOX_HEIGHT}`} preserveAspectRatio="none">
       <Index each={Array.from({ length: PATH_COUNT })}>
         {(_, i) => {
           const y = spacing * (i + 1);
+          //const points = [{ x: 0, y }, { x: VIEWBOX_WIDTH, y }];
           return (
             <path
-              d={zigZagPath([{ x: 0, y }, { x: VIEWBOX_WIDTH, y }], 5, 30)}
-              stroke={i % 2 === 0 ? 'currentColor' : 'currentColor'}
+              d={`M 0 ${y} L ${VIEWBOX_WIDTH} ${y}`}
+              fill="none"
+              onMouseDown={() => setIsPressed(true)}
+              onMouseUp={() => setIsPressed(false)}
+              opacity={0.3 + (i / PATH_COUNT) * 0.7}
               stroke-width="1"
+              stroke={i % 2 === 0 ? 'currentColor' : 'currentColor'}
+              id={`reference-path-${i}`}
+            />
+          );
+        }}
+      </Index>
+      <Index each={Array.from({ length: PATH_COUNT })}>
+        {(_, i) => {
+          const y = spacing * (i + 1);
+          const points = [{ x: 0, y }, { x: VIEWBOX_WIDTH, y }];
+          return (
+            <path
+              d={zigZagPath(points, 5, 30)}
               fill="none"
               opacity={0.3 + (i / PATH_COUNT) * 0.7}
+              stroke-width="1"
+              stroke={i % 2 === 0 ? 'currentColor' : 'currentColor'}
             />
           );
         }}
