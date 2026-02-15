d01a647000a04cbe13d9e7c6f3d90fdf31f60ab8
dotkalm <joelmholmberg@gmail.com>
2026-02-15 12:43:02 -0500

    pass paths from state

--- src/components/Viewbox/index.tsx
+++ src/components/Viewbox/index.tsx
@@ -4,6 +4,8 @@ import {
 } from 'solid-js';
 import { styled } from 'solid-styled-components';
 import { zigZagPath } from '~/utils';
+import type { Point } from '~/types';
+import { INITIAL_PATH_ARRAY } from '~/constants';
 
 const StyledSvg = styled.svg`
   width: 100%;
@@ -18,19 +20,22 @@ const PATH_COUNT = 30;
 export default function Viewbox() {
   const spacing = VIEWBOX_HEIGHT / (PATH_COUNT + 1);
   const [isPressed, setIsPressed] = createSignal(false);
+  const [pathArray, setPathArray] = createSignal<Point[][]>(INITIAL_PATH_ARRAY);
   const handleMouseEnter = () => {
     // figure out what path we are hovering over and then understand the x position 
   }
 
   return (
     <StyledSvg viewBox={`0 0 ${VIEWBOX_WIDTH} ${VIEWBOX_HEIGHT}`} preserveAspectRatio="none">
-      <Index each={Array.from({ length: PATH_COUNT })}>
-        {(_, i) => {
-          const y = spacing * (i + 1);
-          //const points = [{ x: 0, y }, { x: VIEWBOX_WIDTH, y }];
+      <g id="reference-paths">
+      <Index each={pathArray()}>
+        {(line, i) => {
+          const points = line();
+          const [ start ] = points;
+          const end = points[points.length - 1];
           return (
             <path
-              d={`M 0 ${y} L ${VIEWBOX_WIDTH} ${y}`}
+              d={`M ${start.x} ${start.y} L ${end.x} ${end.y}`}
               fill="none"
               onMouseDown={() => setIsPressed(true)}
               onMouseUp={() => setIsPressed(false)}
@@ -42,21 +47,21 @@ export default function Viewbox() {
           );
         }}
       </Index>
-      <Index each={Array.from({ length: PATH_COUNT })}>
-        {(_, i) => {
-          const y = spacing * (i + 1);
-          const points = [{ x: 0, y }, { x: VIEWBOX_WIDTH, y }];
-          return (
-            <path
-              d={zigZagPath(points, 5, 30)}
-              fill="none"
-              opacity={0.3 + (i / PATH_COUNT) * 0.7}
-              stroke-width="1"
-              stroke={i % 2 === 0 ? 'currentColor' : 'currentColor'}
-            />
-          );
-        }}
+      </g>
+      <g id="zigzag-paths">
+      <Index each={pathArray()}>
+        {(points, i) => (
+          <path
+            d={zigZagPath(points(), 5, 30)}
+            fill="none"
+            opacity={0.3 + (i / PATH_COUNT) * 0.7}
+            stroke-width="1"
+            stroke={i % 2 === 0 ? 'currentColor' : 'currentColor'}
+            id={`zigzag-path-${i}`}
+          />
+        )}
       </Index>
+      </g>
     </StyledSvg>
   );
 }
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,15 @@
+import type { Point } from '~/types';
+
 export const BEZIER_APPROXIMATION_FACTOR = 0.3642;
+
+export const VIEWBOX_WIDTH = 1440;
+
+export const VIEWBOX_HEIGHT = 800;
+
+export const PATH_COUNT = 30;
+
+export const INITIAL_PATH_ARRAY: Point[][] = Array.from({ length: PATH_COUNT }).map((_,i) => {
+  const spacing = VIEWBOX_HEIGHT / (PATH_COUNT + 1);
+  const y = spacing * (i + 1);
+  return [{ x: 0, y }, { x: VIEWBOX_WIDTH, y }];
+});
